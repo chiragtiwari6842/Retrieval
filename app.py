33d490157533ea01113c39a5b966b72d1df95749
chiragtiwari6842 <147263512+chiragtiwari6842@users.noreply.github.com>
2024-10-15 10:34:49 +0530

    Update idr

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def play_audio(filename):
     return send_file(file_path, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port, debug=True)
